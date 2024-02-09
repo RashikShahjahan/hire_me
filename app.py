9a48e7debc453ad86848ceb3ae8767e9c6030d21
Rashik Shahjahan <rashikshahjahan@protonmail.com>
2024-02-09 13:16:15 -0500

    test

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def load_data():
     with st.spinner(text="Thinking..."):
         reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
         docs = reader.load_data()
-        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-4", temperature=0.5, system_prompt="You are an expert on Rashik Shahjahan's experience or the services he offers and your job is to answer questions on them to potential clients. Assume that all questions are related to Rashik's experience and services. Keep your answers relevant and based on facts – do not hallucinate features."))
+        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on Rashik Shahjahan's experience or the services he offers and your job is to answer questions on them to potential clients. Assume that all questions are related to Rashik's experience and services. Keep your answers relevant and based on facts – do not hallucinate features."))
         index = VectorStoreIndex.from_documents(docs, service_context=service_context)
         return index
 
